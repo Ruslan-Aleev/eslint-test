a5c77cf748c4ef448363aa3497e81e4ef35a5355
Ruslan-Aleev <aleev.insmile@gmail.com>
2023-04-03 13:59:33 +0400

    eslint & stylelint test

--- assets/components/test/js/web/vanilla/index.js
+++ assets/components/test/js/web/vanilla/index.js
@@ -1,5 +1,12 @@
 const test = 'Test'
 
-// comment
+//comment
 
-console.log(test)
+console.log(test);
+
+console.log(window.hasOwnProperty('document'));
+
+// comment 2
+
+var test2 = 'Test 2';;
+var test3 = 'Test 3';
